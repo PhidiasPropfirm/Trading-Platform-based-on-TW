e08e69a41be8222a7e6c9f20d3939b7aac7dea04
PhidiasPropfirm <159257767+PhidiasPropfirm@users.noreply.github.com>
2025-12-17 00:11:50 +0100

    Add files via upload

--- helpers/delegate.ts
+++ helpers/delegate.ts
@@ -1,30 +1,16 @@
-export type Callback<T1 = void> = (param1: T1) => void;
+import { Callback, ISubscription } from './isubscription';
 
-export interface ISubscription<T1 = void> {
-	subscribe(
-		callback: Callback<T1>,
-		linkedObject?: unknown,
-		singleshot?: boolean
-	): void;
-	unsubscribe(callback: Callback<T1>): void;
-	unsubscribeAll(linkedObject: unknown): void;
-}
-
-interface Listener<T1> {
-	callback: Callback<T1>;
+interface Listener<T1, T2, T3> {
+	callback: Callback<T1, T2, T3>;
 	linkedObject?: unknown;
 	singleshot: boolean;
 }
 
-export class Delegate<T1 = void> implements ISubscription<T1> {
-	private _listeners: Listener<T1>[] = [];
+export class Delegate<T1 = void, T2 = void, T3 = void> implements ISubscription<T1, T2, T3> {
+	private _listeners: Listener<T1, T2, T3>[] = [];
 
-	public subscribe(
-		callback: Callback<T1>,
-		linkedObject?: unknown,
-		singleshot?: boolean
-	): void {
-		const listener: Listener<T1> = {
+	public subscribe(callback: Callback<T1, T2, T3>, linkedObject?: unknown, singleshot?: boolean): void {
+		const listener: Listener<T1, T2, T3> = {
 			callback,
 			linkedObject,
 			singleshot: singleshot === true,
@@ -32,29 +18,21 @@ export class Delegate<T1 = void> implements ISubscription<T1> {
 		this._listeners.push(listener);
 	}
 
-	public unsubscribe(callback: Callback<T1>): void {
-		const index = this._listeners.findIndex(
-			(listener: Listener<T1>) => callback === listener.callback
-		);
+	public unsubscribe(callback: Callback<T1, T2, T3>): void {
+		const index = this._listeners.findIndex((listener: Listener<T1, T2, T3>) => callback === listener.callback);
 		if (index > -1) {
 			this._listeners.splice(index, 1);
 		}
 	}
 
 	public unsubscribeAll(linkedObject: unknown): void {
-		this._listeners = this._listeners.filter(
-			(listener: Listener<T1>) => listener.linkedObject !== linkedObject
-		);
+		this._listeners = this._listeners.filter((listener: Listener<T1, T2, T3>) => listener.linkedObject !== linkedObject);
 	}
 
-	public fire(param1: T1): void {
+	public fire(param1: T1, param2: T2, param3: T3): void {
 		const listenersSnapshot = [...this._listeners];
-		this._listeners = this._listeners.filter(
-			(listener: Listener<T1>) => !listener.singleshot
-		);
-		listenersSnapshot.forEach((listener: Listener<T1>) =>
-			listener.callback(param1)
-		);
+		this._listeners = this._listeners.filter((listener: Listener<T1, T2, T3>) => !listener.singleshot);
+		listenersSnapshot.forEach((listener: Listener<T1, T2, T3>) => listener.callback(param1, param2, param3));
 	}
 
 	public hasListeners(): boolean {
